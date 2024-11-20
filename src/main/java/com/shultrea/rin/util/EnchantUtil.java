38cb4661ac25af84c330990e60f815094a6521e1
fonnymunkey <fonnymunkey@gmail.com>
2024-11-20 13:30:45 -0500

    Vastly simplify Rune Revival handling by moving to mixin

--- src/main/java/com/shultrea/rin/util/EnchantUtil.java
+++ src/main/java/com/shultrea/rin/util/EnchantUtil.java
@@ -2,18 +2,12 @@ package com.shultrea.rin.util;
 
 import com.shultrea.rin.config.ModConfig;
 import com.shultrea.rin.enchantments.curses.EnchantmentPandorasCurse;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
-import net.minecraft.crash.ICrashReportDetail;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.ReportedException;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -169,126 +163,5 @@ public abstract class EnchantUtil {
 			damage -= level * 0.25F + 1.0F;
 		}
 		return damage;
-	}
-
-	//TODO all this nonsense below
-	/**
-	 * Adds the item stack to the inventory, returns false if it is impossible.
-	 */
-	public static boolean addItemStackToInventoryWithoutHolding(ItemStack itemStackIn, InventoryPlayer e) {
-		return addItemStackToInventorySpecificSlot(-1, itemStackIn, e);
-	}
-	
-	public static boolean addItemStackToInventorySpecificSlot(int slot, final ItemStack stack, InventoryPlayer e) {
-		if(stack.isEmpty()) {
-			return false;
-		}
-		else {
-			try {
-				if(stack.isItemDamaged()) {
-					if(slot == -1) {
-						slot = getFirstEmptyStackWithoutHolding(e.mainInventory, e);
-					}
-					if(slot >= 0) {
-						e.mainInventory.set(slot, stack.copy());
-						// ((ItemStack)e.mainInventory.get(slot)).setAnimationsToGo(0);
-						stack.setCount(0);
-						return true;
-					}
-					else if(e.player.capabilities.isCreativeMode) {
-						stack.setCount(0);
-						return true;
-					}
-					else {
-						return false;
-					}
-				}
-				else {
-					int i;
-					while(true) {
-						i = stack.getCount();
-						if(slot == -1) {
-							stack.setCount(storePartialItemStack(stack, e));
-						}
-						else {
-							stack.setCount(addResource(slot, stack, e));
-						}
-						if(stack.isEmpty() || stack.getCount() >= i) {
-							break;
-						}
-					}
-					if(stack.getCount() == i && e.player.capabilities.isCreativeMode) {
-						stack.setCount(0);
-						return true;
-					}
-					else {
-						return stack.getCount() < i;
-					}
-				}
-			}
-			catch(Throwable throwable) {
-				CrashReport crashreport = CrashReport.makeCrashReport(throwable, "Adding item to inventory");
-				CrashReportCategory crashreportcategory = crashreport.makeCategory("Item being added");
-				crashreportcategory.addCrashSection("Item ID", Integer.valueOf(Item.getIdFromItem(stack.getItem())));
-				crashreportcategory.addCrashSection("Item data", Integer.valueOf(stack.getMetadata()));
-				crashreportcategory.addDetail("Registry Name", () -> String.valueOf(stack.getItem().getRegistryName()));
-				crashreportcategory.addDetail("Item Class", () -> stack.getItem().getClass().getName());
-				crashreportcategory.addDetail("Item name", new ICrashReportDetail<String>() {
-					public String call() throws Exception {
-						return stack.getDisplayName();
-					}
-				});
-				throw new ReportedException(crashreport);
-			}
-		}
-	}
-	
-	private static int storePartialItemStack(ItemStack itemStackIn, InventoryPlayer e) {
-		int i = e.storeItemStack(itemStackIn);
-		if(i == -1) {
-			i = getFirstEmptyStackWithoutHolding(e.mainInventory, e);
-		}
-		return i == -1 ? itemStackIn.getCount() : addResource(i, itemStackIn, e);
-	}
-	
-	private static int addResource(int p_191973_1_, ItemStack p_191973_2_, InventoryPlayer e) {
-		Item item = p_191973_2_.getItem();
-		int i = p_191973_2_.getCount();
-		ItemStack itemstack = e.getStackInSlot(p_191973_1_);
-		if(itemstack.isEmpty()) {
-			itemstack = p_191973_2_.copy(); // Forge: Replace Item clone above to preserve item capabilities when picking the item up.
-			itemstack.setCount(0);
-			if(p_191973_2_.hasTagCompound()) {
-				itemstack.setTagCompound(p_191973_2_.getTagCompound().copy());
-			}
-			e.setInventorySlotContents(p_191973_1_, itemstack);
-		}
-		int j = i;
-		if(i > itemstack.getMaxStackSize() - itemstack.getCount()) {
-			j = itemstack.getMaxStackSize() - itemstack.getCount();
-		}
-		if(j > e.getInventoryStackLimit() - itemstack.getCount()) {
-			j = e.getInventoryStackLimit() - itemstack.getCount();
-		}
-		if(j == 0) {
-			return i;
-		}
-		else {
-			i = i - j;
-			itemstack.grow(j);
-			itemstack.setAnimationsToGo(5);
-			return i;
-		}
-	}
-	
-	public static int getFirstEmptyStackWithoutHolding(List list, InventoryPlayer e) {
-		int slot = e.currentItem;
-		for(int i = 0; i < list.size(); ++i) {
-			if(((ItemStack)list.get(i)).isEmpty() && i != slot) {
-				// e.mainInventory.set(i, newTool);
-				return i;
-			}
-		}
-		return -1;
 	}
 }
