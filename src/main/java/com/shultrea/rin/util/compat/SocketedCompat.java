e93eb4aa4403ea15e1e28c07379fbefb8d04f313
Nischhelm <nischhelm@gmail.com>
2025-02-12 00:52:47 +0100

    updated socketed compat

--- src/main/java/com/shultrea/rin/util/compat/SocketedCompat.java
+++ src/main/java/com/shultrea/rin/util/compat/SocketedCompat.java
@@ -2,11 +2,11 @@ package com.shultrea.rin.util.compat;
 
 import net.minecraft.util.text.TextFormatting;
 import socketed.common.config.DefaultJsonConfig;
-import socketed.common.jsondata.GemType;
-import socketed.common.jsondata.entry.RandomValueRange;
-import socketed.common.jsondata.entry.effect.AttributeGemEffect;
-import socketed.common.jsondata.entry.effect.slot.SocketedSlotTypes;
-import socketed.common.jsondata.entry.filter.OreEntry;
+import socketed.common.socket.gem.GemType;
+import socketed.common.socket.gem.effect.slot.SocketedSlotTypes;
+import socketed.common.socket.gem.filter.OreFilter;
+import socketed.common.socket.gem.util.RandomValueRange;
+import socketed.common.socket.gem.effect.AttributeGemEffect;
 
 import java.util.Collections;
 
@@ -14,6 +14,6 @@ public class SocketedCompat {
     public static void registerLapisGem() {
         DefaultJsonConfig.registerDefaultGemType("lapis", new GemType("socketed.tooltip.default.lapis", 2, TextFormatting.BLUE,
                 Collections.singletonList(new AttributeGemEffect(SocketedSlotTypes.ALL, "somanyenchantments.enchantfocus", new RandomValueRange(2, false), 0)),
-                Collections.singletonList(new OreEntry("gemLapis", true))));
+                Collections.singletonList(new OreFilter("gemLapis", true))));
     }
 }
