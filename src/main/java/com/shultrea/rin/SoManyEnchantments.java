50b76711a06448c2c3bdc629b985555571482373
Nischhelm <nischhelm@gmail.com>
2025-05-21 21:37:35 +0200

    bump version 1.0.3
    changelog since 1.0.2
    
    - added enchanting focus attribute (only used in Socketed compat for now)
    - fixed enchantment blacklists
    - fixed some configs not reading from file
    - made burning thorns not do durability dmg when the user is burning
    - added BetterSurvival and SpartanWeaponry compat to remove rlmixins dependency (instead has fermiumbooter dependency now)
    - fixed some enchants being incorrectly classified as treasure/non-treasure
    - made rune revival be applicable on all breakable items instead of only weapons
    - changed penetrating edge and spellbreaker default incompats (now incompat with supremes)
    - fully reworked requirements effects triggering only on full hits (now mostly scaling with attack strength)
    - nerfed combat medic to max x2 healing
    - removed inner berserk vs strengthened vitality incompat
    - added custom canApply type for lycanite equipment
    - fixed adv power not working on crossbows
    - reworked ash destroyer to scale with the burn time of the enemy instead of being incompat with fire aspects
    - reworked enchantment upgrading internally to allow for more customisation via crafttweaker, made GUI better and added JEI plugin

--- src/main/java/com/shultrea/rin/SoManyEnchantments.java
+++ src/main/java/com/shultrea/rin/SoManyEnchantments.java
@@ -32,7 +32,7 @@ public class SoManyEnchantments {
 	
 	public static final String MODID = "somanyenchantments";
 	public static final String NAME = "Rin's So Many Enchantments?";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	public static final Logger LOGGER = LogManager.getLogger(SoManyEnchantments.MODID);
 
 	@Instance(SoManyEnchantments.MODID)
